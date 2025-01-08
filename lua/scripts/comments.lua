b47d4e232b3133a45043d712a5da82e71c3c2ab5
Mikael Forsberg <mkforsb@gmail.com>
2025-01-08 08:24:18 +0100

    feat(scripts.comments): support two-part commentstrings

--- lua/scripts/comments.lua
+++ lua/scripts/comments.lua
@@ -6,23 +6,23 @@ local quote = function(text)
     return text:gsub(quotepattern, '%%%1')
 end
 
-local toggle_line = function(text, indent, commentstring, commentstring_quoted)
+local toggle_line = function(text, indent, commentpre, commentpost, commentpre_quoted, commentpost_quoted)
     if text:match('[^%s]') == nil then
         return text
     end
 
-    if text:match('^%s*' .. commentstring_quoted) == nil then
+    if text:match('^%s*' .. commentpre_quoted) == nil then
         local replacement
 
         if indent == '' then
-            replacement, _ = commentstring .. text
+            replacement, _ = commentpre .. text .. commentpost
         else
-            replacement, _ = text:gsub('^(' .. indent .. ')(%s*)(.*)', '%1' .. commentstring .. '%2%3')
+            replacement, _ = text:gsub('^(' .. indent .. ')(%s*)(.*)', '%1' .. commentpre .. '%2%3' .. commentpost)
         end
 
         return replacement
     else
-        local replacement, _ = text:gsub('^(%s*)' .. commentstring_quoted, '%1')
+        local replacement, _ = text:gsub('^(%s*)' .. commentpre_quoted, '%1'):gsub(commentpost_quoted .. '$', '')
         return replacement
     end
 end
@@ -33,7 +33,8 @@ M.toggle_line_comment = function()
         return
     end
 
-    local commentstring = vim.bo.commentstring:gsub('%%s', '')
+    local commentpre = vim.bo.commentstring:gsub('%%s.*', '')
+    local commentpost = vim.bo.commentstring:gsub('.*%%s', '')
     local cursorline = vim.api.nvim_win_get_cursor(0)[1]
     local linetext = vim.api.nvim_buf_get_lines(0, cursorline - 1, cursorline, false)[1]
     local indent = linetext:match('^(%s*)')
@@ -43,7 +44,7 @@ M.toggle_line_comment = function()
         cursorline - 1,
         cursorline,
         false,
-        { toggle_line(linetext, indent, commentstring, quote(commentstring)) }
+        { toggle_line(linetext, indent, commentpre, commentpost, quote(commentpre), quote(commentpost)) }
     )
 end
 
@@ -53,8 +54,10 @@ M.toggle_selection_comment = function()
         return
     end
 
-    local commentstring = vim.bo.commentstring:gsub('%%s', '')
-    local commentstring_quoted = quote(commentstring)
+    local commentpre = vim.bo.commentstring:gsub('%%s.*', '')
+    local commentpost = vim.bo.commentstring:gsub('.*%%s', '')
+    local commentpre_quoted = quote(commentpre)
+    local commentpost_quoted = quote(commentpost)
     local sel_start = vim.fn.getpos('v')[2]
     local sel_end = vim.fn.getcurpos()[2]
 
@@ -81,7 +84,7 @@ M.toggle_selection_comment = function()
     end
 
     for i = 1, #text do
-        text[i] = toggle_line(text[i], min_indent, commentstring, commentstring_quoted)
+        text[i] = toggle_line(text[i], min_indent, commentpre, commentpost, commentpre_quoted, commentpost_quoted)
     end
 
     vim.api.nvim_buf_set_lines(0, sel_start - 1, sel_end, false, text)
