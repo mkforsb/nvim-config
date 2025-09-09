6d125f02ac56634072d987c4fa886e90babaf4b6
Mikael Forsberg <mikael.forsberg@stim.se>
2025-09-09 20:31:23 +0200

    fix: neode_transparency deprecated, use _opacity

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -9,7 +9,7 @@ if vim.g.neovide then
     vim.g.neovide_light_angle_degrees = 45
     vim.g.neovide_light_radius = 5
 
-    vim.g.neovide_transparency = 0.95
+    vim.g.neovide_opacity = 0.95
     vim.g.neovide_scroll_animation_length = 0.02
     vim.g.neovide_cursor_animation_length = 0
     vim.g.neovide_cursor_trail_size = 0
